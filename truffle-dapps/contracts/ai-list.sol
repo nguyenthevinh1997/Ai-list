9392b24dd7f2628ba34fe4c733598b7a62c8893b
Vinh <nguyenthevinh1997@gmail.com>
2019-05-20 16:56:13 +0700

    Finish code

--- truffle-dapps/contracts/ai-list.sol
+++ truffle-dapps/contracts/ai-list.sol
@@ -7,20 +7,31 @@ contract AIList {
     }
     event newTask(uint id, string name, bool completed);
     Task[] public tasks;
+    mapping (uint => address) taskToOwner;
+    mapping (address => uint) ownerTaskCounter;
     function saveTask(string memory _name, bool _completed) public {
         uint id = tasks.push(Task(_name, _completed))-1;
         // ...taskToOwnerへの処理。
         // ...ownerTaskCounterへの処理。
+        taskToOwner[id] = msg.sender;
+        ownerTaskCounter[msg.sender]++;
+        tasks.push(Task(_name, _completed));
         emit newTask(id, _name, _completed);
     }
-    mapping (uint => address) taskToOwner;
-    mapping (address => uint) ownerTaskCounter;
     function getTaskId(address _owner) public view returns(uint[] memory){
-        uint[] memory result = ;
-　　　　 return result;
+        uint[] memory result = new uint[](ownerTaskCounter[_owner]);
+        uint counter = 0;
+        for (uint i = 0; i < tasks.length; i++) {
+        if (taskToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+        }
+    }
+    return result;
     }
     function completeTask(uint _taskId) public {
-        require(msg.sender == taskToOwner[_taskId]);
+        require(msg.sender == taskToOwner[_taskId],"Check ID");
+        tasks[_taskId].completed = true;
     }
 
 }
